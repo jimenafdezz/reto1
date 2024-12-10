36e70782b00135d2f02092699866da8772913edc
jimenafdezz <jimena.fernandezlabrador@iesmiguelherrero.com>
2024-12-10 17:31:52 +0100

    reto

---
+++ ValorarReto/src/main/java/com/mycompany/valorarreto/ValorarReto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.valorarreto;
+
+/**
+ *
+ * @author daw105
+ */
+public class ValorarReto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
