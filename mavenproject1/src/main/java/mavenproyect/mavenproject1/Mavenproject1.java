f432f93451b722aa3d25d878b18a9b653a67e873
jimenafdezz <jimena.fernandezlabrador@iesmiguelherrero.com>
2025-03-07 17:18:55 +0100

    Iñigo el de AmpueroO

---
+++ mavenproject1/src/main/java/mavenproyect/mavenproject1/Mavenproject1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mavenproyect.mavenproject1;
+
+/**
+ *
+ * @author jimen
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("mi gente");
+    }
+}
